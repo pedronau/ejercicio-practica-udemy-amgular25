7fe8ab3e990bbae8919c7f89e86cd2d93631d874
pedronau <pedropefer34@gmail.com>
2025-03-14 11:56:06 +0100

    feat: conseguimos que el array annualData se llene de datos (se hace console.log y sale bien). ahora hay que trabajar con ellos.

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
@@ -11,8 +12,37 @@ export class InputComponent {
 
   @Output() botonPulsado = new EventEmitter<boolean>();
 
+  inputInversionInicial: number = 0;
+  inputAportacionAnual: number = 0;
+  inputTasaRetorno: number = 0;
+  inputDuracion: number = 0;
+
   datosIntroducidos() {
     this.botonPulsado.emit(true);
+  }
+
+  calculateInvestmentResults() {
+    const annualData = [];
+    let investmentValue = this.inputInversionInicial;
+
+    for (let i = 0; i < this.inputDuracion; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (this.inputTasaRetorno / 100);
+      investmentValue += interestEarnedInYear + this.inputAportacionAnual;
+      const totalInterest =
+        investmentValue - this.inputAportacionAnual * year - this.inputInversionInicial;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: this.inputAportacionAnual,
+        totalInterest: totalInterest,
+        totalAmountInvested: this.inputInversionInicial + this.inputAportacionAnual * year,
+      });
+    }
+  
+    // return annualData;
+    return console.log(annualData);
   }
 
 }
