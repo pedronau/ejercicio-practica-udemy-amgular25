81b2a201af6f56c94bf6afe9b25cbf7ce91e211f
pedronau <pedropefer34@gmail.com>
2025-03-14 10:03:11 +0100

    style: ya tenemos el estilo de nuestros dos primeros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InputComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
